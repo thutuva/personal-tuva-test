c7efbe2b0888349f0fe2a0bb812d0019951e35e3
thutuva <101137626+thutuva@users.noreply.github.com>
2023-12-20 11:47:11 -0500

    Update generate_markdown.py

--- .github/generate_markdown.py
+++ .github/generate_markdown.py
@@ -4,10 +4,7 @@ def fetch_yaml_local(yaml_path):
     with open(yaml_path, 'r') as file:
         return file.read()
 
-def generate_markdown(yaml_path, markdown_path):
-    with open(yaml_path, 'r') as file:
-        data = yaml.safe_load(file)
-
+def generate_markdown(data):
     # Generate Markdown content from the YAML data
     markdown_content = f"# {data.get('name', 'Core Data Model')}\n\n"
     markdown_content += f"## Description\n\n{data.get('description', 'No description')}\n\n"
@@ -24,7 +21,7 @@ def generate_markdown(yaml_path, markdown_path):
         # Add a table header for the model columns
         markdown_content += "| Name | Description |\n"
         markdown_content += "| ---- | ----------- |\n"
-
+        
         # Loop through each column in the 'columns' section for the model
         for column in model.get('columns', []):
             # Extract the column name and description
@@ -33,14 +30,17 @@ def generate_markdown(yaml_path, markdown_path):
             # Add a table row with the column name and description
             markdown_content += f"| {name} | {description} |\n"
 
+    return markdown_content
+
 def write_markdown_local(markdown_path, markdown_content):
     # Write the generated Markdown content to the specified Markdown file
     with open(markdown_path, 'w') as markdown_file:
         markdown_file.write(markdown_content)
 
 yaml_path = 'models/core/core_models.yml'
-markdown_path = '/.github/output.md'
+markdown_path = '.github/output.md'
 
 yaml_content = fetch_yaml_local(yaml_path)
-markdown_content = generate_markdown(yaml_content)
+data = yaml.safe_load(yaml_content)
+markdown_content = generate_markdown(data)
 write_markdown_local(markdown_path, markdown_content)
